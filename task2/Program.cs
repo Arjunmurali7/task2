564802db9b3e2e2bed5303ea8d6a5ba3d501dce9
Arjunmurali7 <146719835+Arjunmurali7@users.noreply.github.com>
2025-09-08 17:53:29 +0530

    Add project files.

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace StudentApp
+{
+    class Student
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+
+
+            Console.Write("Enter Student Name: ");
+            student1.Name = Console.ReadLine();
+
+            Console.Write("Enter Student Age: ");
+            student1.Age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nStudent Details:");
+            student1.PrintDetails();
+
+            Console.ReadLine();
+        }
+    }
+}
